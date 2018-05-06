c767cf32d81772f5e6f48234c9d627f33190f6f9
loiclefevre <loic.lefevre@gmail.com>
2018-05-06 16:19:31 -0400

    Update vm.tf

--- oracle/oci/vm.tf
+++ oracle/oci/vm.tf
@@ -101,3 +101,44 @@ resource "oci_core_subnet" "SN-SB-Data" {
   dhcp_options_id = "${oci_core_virtual_network.VCN-SB.default_dhcp_options_id}"
 }
 
+variable "ImageOCID" {
+    type = "map"
+    default = {
+        // See https://docs.us-phoenix-1.oraclecloud.com/Content/Resources/Assets/OracleProvidedImageOCIDs.pdf
+        // Oracle-provided image "Oracle-Linux-7.4-2018.02.21-1"
+        us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaupbfz5f5hdvejulmalhyb6goieolullgkpumorbvxlwkaowglslq"
+        us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaajlw3xfie2t5t52uegyhiq2npx7bqyu4uvi2zyu3w3mqayc2bxmaa"
+        eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt1.aaaaaaaa7d3fsb6272srnftyi4dphdgfjf6gurxqhmv6ileds7ba3m2gltxq"
+        uk-london-1 = "ocid1.image.oc1.uk-london1.aaaaaaaaa6h6gj6v4n56mqrbgnosskq63blyv2752g36zerymy63cfkojiiq"
+    }
+}
+
+resource "oci_core_instance" "SB-Client" {
+    availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.data_store["AD"]],"name")}" 
+    compartment_id = "${var.compartment_ocid}"
+    display_name = "SN-SB-CLIENT-${var.sbName}"
+    hostname_label = "client"
+    source_details {
+        source_type = "image"
+        source_id = "${var.ImageOCID[var.region]}"
+    }    
+shape = "VM.Standard1.2"
+    subnet_id = "${oci_core_subnet.SN-SB-Data.id}"
+    extended_metadata {
+        ssh_authorized_keys = "${var.ssh_public_key}"
+    }
+}
+
+# Output the private and public IPs of the instance
+output "SB-Client-PrivateIPs" {
+  value = ["${oci_core_instance.SB-Client.*.private_ip}"]
+}
+
+output "SB-Client-PublicIPs" {
+  value = ["${oci_core_instance.SB-Client.*.public_ip}"]
+}
+
+# Output the boot volume IDs of the instance
+output "SB-Client-BootVolumeIDs" {
+  value = ["${oci_core_instance.SB-Client.*.boot_volume_id}"]
+}
